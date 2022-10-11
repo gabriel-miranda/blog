91b439b0e26cc25262fca61774a158ca18d1cfa6
gabriel-miranda <gabmirdev@gmail.com>
2022-10-11 00:38:01 -0300

    Try to fix prod routing

--- next.config.js
+++ next.config.js
@@ -1,10 +1,23 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
+  assetPrefix: isProd ? 'https://gabmirdev.com' : undefined,
   reactStrictMode: true,
   swcMinify: true,
   images: {
     unoptimized: true,
     domains: ['images.ctfassets.net'],
+  },
+  async rewrites() {
+    return isProd
+      ? [
+          {
+            source: '/:path*',
+            destination: '/blog/:path*',
+          },
+        ]
+      : [];
   },
 };
 
